a013a5893d6ff53d1076a26baa85a9f2dd68a672
Sarah Young <y.sarahc@gmail.com>
2014-02-19 20:08:40 +0000

    Created error for learning experience

--- hello.rb
+++ hello.rb
@@ -1,7 +1,16 @@
+=begin
 def hello(name, age)
 	puts "Welcome #{name}, #{age} is definitely not too old to learn how to code."
 end
 
 hello("rob",30)
 hello("jane",72)
-hello("pete",55)
+hello("jenny",55)
+=end
+
+def buy(product)
+	puts "Here is your #{product}, thank you"
+end
+
+buy() # no argument given, error raised
+
